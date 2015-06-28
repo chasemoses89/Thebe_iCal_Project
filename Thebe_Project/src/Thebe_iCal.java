0f77cf9b26cd0b7f2841cda022fa0929134aaa74
David Igeta <digeta808@gmail.com>
2015-06-27 14:41:31 -1000

    Added a multi input dialog box to enter information

--- Thebe_Project/src/Thebe_iCal.java
+++ Thebe_Project/src/Thebe_iCal.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.io.*;
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 /**
  * 6/27/2015
@@ -10,13 +11,50 @@ import javax.swing.JOptionPane;
 public class Thebe_iCal {
 
 	public static void main(String[] args) {
+		//Event subject line
+		String sSubject = "";
+		//Event location
+		String sLocation = "";
+		//Event description which will be placed in the body
+		String sBody = "";
+		//Beginning month event starts
+		int iStartMonth;
+		//Beginning day event starts
+		int iStartDay;
+		//Beginning year event starts
+		int iStartYear;
+		//End month event ends
+		int iEndMonth;
+		//End day event ends
+		int iEndDay;
+		//End year event ends
+		int iEndYear;
+		//sentinel value
+		boolean bContinue = false;
+		
+		JTextField fSubject = new JTextField();
+		JTextField fLocation = new JTextField();
+		JTextField fDescription = new JTextField();
+		
+		Object[] fields = {
+			"Subject", fSubject,
+			"Location", fLocation,
+			"Description", fDescription
+		};
+		
+		JOptionPane.showConfirmDialog(null, fields, "iCal Event Creator", JOptionPane.OK_CANCEL_OPTION);
+		
+		sSubject = fSubject.getText();
+		sLocation = fLocation.getText();
+		sBody = fDescription.getText();
+
 		//initializes fileWriter
 		PrintWriter fileWriter = null;
 		try {
 			//makes connection to fileWriter and output file
 			fileWriter = new PrintWriter("iCal_Event.ics");
 			//begin writing to file
-			//every line that is commented out is optional and can still be read/processed by MS Outllok 2010
+			//every line that is commented out is optional and can still be read/processed by MS Outlook 2010
 			fileWriter.println("BEGIN:VCALENDAR\n"
 					+ "PRODID:-//Microsoft Corporation//Outlook 14.0 MIMEDIR//EN\n"
 					+ "VERSION:2.0\n"
@@ -33,15 +71,15 @@ public class Thebe_iCal {
 					+ "BEGIN:VEVENT\n"
 					+ "CLASS:PUBLIC\n"
 					+ "CREATED:20150627T031544Z\n"
-					+ "DESCRIPTION:This is an example of sample text in the body of an appointment and then modified.\\n\n"
+					+ "DESCRIPTION:" + sBody + "\\n\n"
 					+ "DTEND;TZID=\"Hawaiian Standard Time\":20150817T140000\n"
 					+ "DTSTAMP:20150627T031544Z\n"
 					+ "DTSTART;TZID=\"Hawaiian Standard Time\":20150817T130000\n"
 					+ "LAST-MODIFIED:20150627T031544Z\n"
-					+ "LOCATION:Hamilton Library\n"
+					+ "LOCATION:" + sLocation + "\n"
 					+ "PRIORITY:5\n"
 					+ "SEQUENCE:0\n"
-					+ "SUMMARY;LANGUAGE=en-us:Study for ICS 314 Exam\n"
+					+ "SUMMARY;LANGUAGE=en-us:" + sSubject + "\n"
 					+ "TRANSP:OPAQUE\n"
 					+ "UID:040000008200E00074C5B7101A82E00800000000407663BC33B0D001000000000000000\n"
 					+ "\t010000000109752F585EB3B448B59159DFD3CF4D2\n"
@@ -67,7 +105,7 @@ public class Thebe_iCal {
 			//closes fileWriter
 			fileWriter.close();
 			
-		}
+		}//end try
 		//this exception must be caught in order to use the fileWriter
 		catch (FileNotFoundException fnf) {
 			JOptionPane.showMessageDialog(null, "The file could not be found!");
@@ -75,6 +113,6 @@ public class Thebe_iCal {
 		//displays message if file was written successfully check if changes were made
 		JOptionPane.showMessageDialog(null,"Content has been successfully written to file!");
 
-	}
+	}//end main() arg
 
-}
+}//ends Public Class Thebe_iCal
